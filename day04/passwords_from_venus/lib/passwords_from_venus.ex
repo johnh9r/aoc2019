4624a89547bdfc8a375ef6d6f7735bd43d5b6fe6
John Hagemeister <johnh9r@posteo.net>
2019-12-04 08:53:38 +0100

    d04p2: full solution (WIP)

--- day04/passwords_from_venus/lib/passwords_from_venus.ex
+++ day04/passwords_from_venus/lib/passwords_from_venus.ex
@@ -8,15 +8,15 @@ defmodule PasswordsFromVenus do
 
   @doc """
   iex> PasswordsFromVenus.count_conformant_passwords_in_range("111111-111111")
-  1
+  {1, ["111111"]}
 
   iex> PasswordsFromVenus.count_conformant_passwords_in_range("223450-223450")
-  0
+  {0, []}
 
   iex> PasswordsFromVenus.count_conformant_passwords_in_range("123789-123789")
-  0
+  {0, []}
   """
-  @spec count_conformant_passwords_in_range(String.t()) :: integer
+  @spec count_conformant_passwords_in_range(String.t()) :: tuple
   def count_conformant_passwords_in_range(range_s) do
     [lo, hi] =
       range_s
@@ -30,8 +30,38 @@ defmodule PasswordsFromVenus do
       |> Enum.map(&Integer.to_string/1)
       |> Enum.filter(fn w -> String.match?(w, ~r<(.)\1>) && monotonically_increasing_digits(w) end)
 
-    length(conformant_passwords)
-    end
+    {length(conformant_passwords), conformant_passwords}
+  end
+
+  @doc """
+  iex> PasswordsFromVenus.count_strictly_conformant_passwords_in_range("112233-112233")
+  {1, ["112233"]}
+
+  iex> PasswordsFromVenus.count_strictly_conformant_passwords_in_range("123444-123444")
+  {0, []}
+
+  iex> PasswordsFromVenus.count_strictly_conformant_passwords_in_range("111122-111122")
+  {1, ["111122"]}
+  """
+  @spec count_strictly_conformant_passwords_in_range(String.t()) :: tuple
+  def count_strictly_conformant_passwords_in_range(range_s) do
+    {_, minimally_conformant_passwords} =
+      count_conformant_passwords_in_range(range_s)
+
+    strictly_conformant_passwords =
+      minimally_conformant_passwords
+      |> Enum.filter(fn w -> strictly_conforming?(w) end)
+
+    {length(strictly_conformant_passwords), strictly_conformant_passwords}
+  end
+
+  @spec strictly_conforming?(String.t()) :: boolean
+  defp strictly_conforming?(w) do
+    Regex.scan(~r/(.)\1/, w)
+    |> List.flatten()
+    |> Enum.filter(fn s -> String.length(s) == 2 end)
+    |> Enum.any?(fn s -> c = String.at(s, 0); !String.match?(w, ~r/#{c}{3,}/x) end)
+  end
 
   @spec monotonically_increasing_digits(String.t()) :: boolean
   defp monotonically_increasing_digits(w) do
--- day04/passwords_from_venus/test/passwords_from_venus_test.exs
+++ day04/passwords_from_venus/test/passwords_from_venus_test.exs
@@ -8,7 +8,13 @@ defmodule PasswordsFromVenusTest do
     [password_range: candidate_password_range]
   end
 
-  test "personal input processed correctly", context do
-    assert PasswordsFromVenus.count_conformant_passwords_in_range(context[:password_range]) == 925
+  test "(part 1) personal input processed correctly", context do
+    {n, ws} = PasswordsFromVenus.count_conformant_passwords_in_range(context[:password_range])
+    assert {n, ws |> Enum.take(5)} ==  {925, ["277777", "277778", "277779", "277788", "277789"]}
+  end
+
+  test "(part 2) personal input processed correctly", context do
+    {n, ws} = PasswordsFromVenus.count_strictly_conformant_passwords_in_range(context[:password_range])
+    assert {n, ws |> Enum.take(5)} ==  {607, ["277788", "277799", "277888", "277889", "277899"]}
   end
 end
