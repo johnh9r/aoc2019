896b3a55ce65d533127a38b2b66579daf2e2d576
John Hagemeister <johnh9r@posteo.net>
2019-12-07 17:42:10 +0100

    day07p1: complete

---
+++ day07/amps/lib/amps.ex
@@ -0,0 +1,70 @@
+defmodule Amps do
+  @moduledoc """
+  determine maximum achievable thrust from exhaustive settings permutations across five amplifiers
+  """
+
+  @settings_pt1 [0, 1, 2, 3, 4]
+
+  @doc """
+  iex> Amps.max_thrust([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])
+  {43210, "43210"}
+
+  iex> Amps.max_thrust([3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0])
+  {54321, "01234"}
+
+  iex> Amps.max_thrust([3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0])
+  {65210, "10432"}
+  """
+  @spec max_thrust([integer]) :: tuple
+  def max_thrust(firmware) do
+    permutations(@settings_pt1)
+    |> Enum.map(
+      fn [phase_a, phase_b, phase_c, phase_d, phase_e] = phases ->
+        # by problem definition, first input is zero
+        {line_out_a, _} = IntCodeDoublePlus.execute(firmware, [phase_a, 0])
+        {line_out_b, _} = IntCodeDoublePlus.execute(firmware, [phase_b, line_out_a])
+        {line_out_c, _} = IntCodeDoublePlus.execute(firmware, [phase_c, line_out_b])
+        {line_out_d, _} = IntCodeDoublePlus.execute(firmware, [phase_d, line_out_c])
+        {line_out_e, _} = IntCodeDoublePlus.execute(firmware, [phase_e, line_out_d])
+        {
+          line_out_e,
+          phases |> Enum.map_join("", &Integer.to_string/1)
+        }
+      end
+    )
+    |> Enum.max_by(fn {thrust, phases} -> thrust end)
+  end
+
+  @doc """
+  iex> Amps.permutations([])
+  [[]]
+
+  iex> Amps.permutations([1])
+  [[1]]
+
+  iex> Amps.permutations([1, 2])
+  [[1, 2], [2, 1]]
+
+  iex> Amps.permutations([1, 2, 3])
+  [
+    [1, 2, 3],
+    [1, 3, 2],
+    [2, 1, 3],
+    [2, 3, 1],
+    [3, 1, 2],
+    [3, 2, 1]
+  ]
+  """
+  @spec permutations([integer]) :: [[integer]]
+  def permutations([]), do: [[]]
+  def permutations([value]), do: [[value]]
+  def permutations(values) do
+    Range.new(1, length(values))
+    |> Enum.flat_map(
+      fn i ->
+        {this, others} = List.pop_at(values, i - 1)
+        Enum.map(permutations(others), fn os -> [this | os] end)
+      end
+    )
+  end
+end
