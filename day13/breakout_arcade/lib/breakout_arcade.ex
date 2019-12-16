c3b344f91190262493a308174783e69d69aefe51
John Hagemeister <johnh9r@posteo.net>
2019-12-16 01:24:36 +0100

    d13p2: tentative steps ...

--- day13/breakout_arcade/lib/breakout_arcade.ex
+++ day13/breakout_arcade/lib/breakout_arcade.ex
@@ -1,195 +1,203 @@
+defmodule BreakoutArcade.WorldAffairs do
+  @moduledoc """
+  maintain state _between_ callbacks from IntCode[Boost] robot process
+  """
+
+  # supervisor trees not required in this scenario
+  # use Agent
+
+  # next (output) action
+  @do_coord_x :coord_x
+  @do_coord_y :coord_y
+  @do_paint :paint
+  @do_score_noop :score_noop
+  @do_score_value :score_value
+
+  # invalid x-coordinate: escape for updating score w/ following value
+  @coord_esc -1
+
+  # tile_id encoding from problem definition
+  @empty  0
+  @wall   1
+  @block  2
+  @paddle 3
+  @ball   4
+
+  @joy_neutral 0
+  @joy_left -1
+  @joy_right 1
+
+  @doc """
+  """
+  @spec initialize(Keyword.t()) :: pid
+  def initialize(initial_state) do
+    Agent.start_link(fn -> initial_state end, name: __MODULE__)
+  end
+
+  @doc """
+  """
+  @spec get_final_state() :: Keyword.t()
+  def get_final_state() do
+    Agent.get(__MODULE__, fn state -> state end)
+  end
+
+  @doc """
+  # iex> RoboPaintWorld.WorldAffairs.handle_input_request()
+  """
+  @spec handle_input_request() :: {integer}
+  def handle_input_request() do
+    # restore context to answer question
+    kw = Agent.get(__MODULE__, fn state -> state end)
+    # tiles =
+    #   Keyword.fetch!(kw, :tiles)
+    #   |> render_screen()
+    #   |> (fn screen -> ["\n\n" | screen] end).()
+    #   |> IO.write()
+
+    [joy_move | remaining_joy_moves] = Keyword.fetch!(kw, :joy_moves)
+
+    Agent.update(
+      __MODULE__,
+      fn state -> Keyword.merge(state, [joy_moves: remaining_joy_moves]) end
+    )
+
+    joy_move
+  end
+
+  @doc """
+  # iex> BreakoutArcade.WorldAffairs.handle_output_request()
+  """
+  # 
+  @spec handle_output_request(integer) :: :ok
+  def handle_output_request(value) do
+    # restore context to change world
+    kw = Agent.get(__MODULE__, fn state -> state end)
+    action = Keyword.fetch!(kw, :next_action)
+
+    # in this scenario (single client), no safety or performance concerns
+    # over executing functions on agent
+    case action do
+      @do_coord_x ->
+        Agent.update(
+          __MODULE__,
+          fn state ->
+            buffer = Keyword.fetch!(state, :buffer)
+            # sanity check
+            0 = length(buffer)
+
+            case value do
+              @coord_esc ->
+                Keyword.merge(state, [next_action: @do_score_noop])
+
+              _ ->
+                Keyword.merge(state, [buffer: [value], next_action: @do_coord_y, ]) end
+          end
+        )
+
+      @do_coord_y ->
+        Agent.update(
+          __MODULE__,
+          fn state ->
+            buffer = Keyword.fetch!(state, :buffer)
+            # sanity check
+            1 = length(buffer)
+            Keyword.merge(state, [buffer: buffer ++ [value], next_action: @do_paint]) end)
+
+      @do_paint ->
+        Agent.update(
+          __MODULE__,
+          fn state ->
+            tiles = Keyword.fetch!(state, :tiles)
+            [x, y] = Keyword.fetch!(state, :buffer)
+
+            # overwrite (w/o maintaining history)
+            new_tiles = Map.put(tiles, {x, y}, value)
+
+            new_tiles
+            |> render_screen()
+            |> (fn screen -> if true || Keyword.fetch!(state, :score) != 0, do: ["\n\n" | screen], else: "" end).()
+            |> IO.write()
+
+            Keyword.merge(state, [tiles: new_tiles, buffer: [], next_action: @do_coord_x]) end)
+
+      @do_score_noop ->
+        Agent.update(
+          __MODULE__,
+          fn state -> Keyword.merge(state, [next_action: @do_score_value]) end
+        )
+
+      @do_score_value ->
+        Agent.update(
+          __MODULE__,
+          fn state -> Keyword.merge(state, [score: value, next_action: @do_coord_x]) end
+        )
+        IO.inspect(value, label: "\nscore")
+    end
+    :ok
+  end
+
+  @spec render_screen(%{required({integer, integer}) => integer}) :: iodata
+  defp render_screen(tiles) do
+    tiles
+    # use y-coordinate as dominant sort key in order to form scan lines
+    |> Enum.group_by(fn {{_x, y}, _tile_id} -> y end)
+    |> Enum.into(
+      [],
+      fn {_group_key_y, tiles} ->
+        tiles
+        |> Enum.sort_by(fn {{x, y},_} -> x end)
+        |> Enum.map(fn {{x,y}, tile_id} -> tile_id end)
+      end
+    )
+    |> Enum.map(
+      fn tiles_in_scan_line ->
+        tiles_in_scan_line
+        |> Enum.reduce(
+          [],
+          fn tile_id, acc ->
+            tile_ch =
+              case tile_id do
+                @empty -> " "
+                @wall -> "#"
+                @block -> "*"
+                @paddle -> "^"
+                @ball -> "O"
+                x -> raise "unknown tile_id #{x}"
+              end
+
+            [tile_ch | acc]
+          end
+        )
+      end
+    )
+    |> Enum.map(&Enum.reverse/1)
+    |> Enum.intersperse("\n")
+  end
+
+  # auxiliary functions to share select constants with enclosing module
+  def coord_x, do: @do_coord_x
+  def coord_y, do: @do_coord_y
+  def paint, do: @do_paint
+
+  def empty, do: @empty
+  def wall, do: @wall
+  def block, do: @block
+  def paddle, do: @paddle
+  def ball, do: @ball
+
+  def j_, do: @joy_neutral
+  def jl, do: @joy_left
+  def jr, do: @joy_right
+end
+
+
 defmodule BreakoutArcade do
   @moduledoc """
   model environment for given IntCode[Boost] robot program
   """
 
-  # vvv
-
-  defmodule WorldAffairs do
-    @moduledoc """
-    maintain state _between_ callbacks from IntCode[Boost] robot process
-    """
-
-    # supervisor trees not required in this scenario
-    # use Agent
-
-    # next (output) action
-    @do_coord_x :coord_x
-    @do_coord_y :coord_y
-    @do_paint :paint
-    @do_score_noop :score_noop
-    @do_score_value :score_value
-
-    # invalid x-coordinate: escape for updating score w/ following value
-    @coord_esc -1
-
-    # tile_id encoding from problem definition
-    @empty  0
-    @wall   1
-    @block  2
-    @paddle 3
-    @ball   4
-
-    @j_ 0
-    @jl -1
-    @jr 1
-
-    @doc """
-    """
-    @spec initialize(Keyword.t()) :: pid
-    def initialize(initial_state) do
-      Agent.start_link(fn -> initial_state end, name: __MODULE__)
-    end
-
-    @doc """
-    """
-    @spec get_final_state() :: Keyword.t()
-    def get_final_state() do
-      Agent.get(__MODULE__, fn state -> state end)
-    end
-
-    @doc """
-    # iex> RoboPaintWorld.WorldAffairs.handle_input_request()
-    """
-    @spec handle_input_request() :: {integer}
-    def handle_input_request() do
-      # restore context to answer question
-      kw = Agent.get(__MODULE__, fn state -> state end)
-      tiles =
-        Keyword.fetch!(kw, :tiles)
-        |> render_screen()
-        |> (fn screen -> ["\n\n" | screen] end).()
-        |> IO.write()
-
-      [joy_move | remaining_joy_moves] = Keyword.fetch!(kw, :joy_moves)
-
-      Agent.update(
-        __MODULE__,
-        fn state -> Keyword.merge(state, [joy_moves: remaining_joy_moves]) end
-      )
-
-      joy_move
-    end
-
-    @doc """
-    # iex> BreakoutArcade.WorldAffairs.handle_output_request()
-    """
-    # 
-    @spec handle_output_request(integer) :: :ok
-    def handle_output_request(value) do
-      # restore context to change world
-      kw = Agent.get(__MODULE__, fn state -> state end)
-      action = Keyword.fetch!(kw, :next_action)
-
-      # in this scenario (single client), no safety or performance concerns
-      # over executing functions on agent
-      case action do
-        @do_coord_x ->
-          Agent.update(
-            __MODULE__,
-            fn state ->
-              buffer = Keyword.fetch!(state, :buffer)
-              # sanity check
-              0 = length(buffer)
-
-              case value do
-                @coord_esc ->
-                  Keyword.merge(state, [next_action: @do_score_noop])
-
-                _ ->
-                  Keyword.merge(state, [buffer: [value], next_action: @do_coord_y, ]) end
-            end
-          )
-
-        @do_coord_y ->
-          Agent.update(
-            __MODULE__,
-            fn state ->
-              buffer = Keyword.fetch!(state, :buffer)
-              # sanity check
-              1 = length(buffer)
-              Keyword.merge(state, [buffer: buffer ++ [value], next_action: @do_paint]) end)
-
-        @do_paint ->
-          Agent.update(
-            __MODULE__,
-            fn state ->
-              tiles = Keyword.fetch!(state, :tiles)
-              [x, y] = Keyword.fetch!(state, :buffer)
-
-              # overwrite (w/o maintaining history)
-              new_tiles = Map.put(tiles, {x, y}, value)
-              # IO.inspect({x,y,value}, label: "\n")
-
-              Keyword.merge(state, [tiles: new_tiles, buffer: [], next_action: @do_coord_x]) end)
-
-        @do_score_noop ->
-          Agent.update(
-            __MODULE__,
-            fn state -> Keyword.merge(state, [next_action: @do_score_value]) end
-          )
-
-        @do_score_value ->
-          Agent.update(
-            __MODULE__,
-            fn state -> Keyword.merge(state, [score: value, next_action: @do_coord_x]) end
-          )
-          IO.inspect(value, label: "\nscore")
-      end
-      :ok
-    end
-
-    @spec render_screen(%{required({integer, integer}) => integer}) :: iodata
-    defp render_screen(tiles) do
-      tiles
-      # use y-coordinate as dominant sort key in order to form scan lines
-      |> Enum.group_by(fn {{_x, y}, _tile_id} -> y end)
-      |> Enum.into(
-        [],
-        fn {_group_key_y, tiles} ->
-          tiles
-          |> Enum.sort_by(fn {{x, y},_} -> x end)
-          |> Enum.map(fn {{x,y}, tile_id} -> tile_id end)
-        end
-      )
-      |> Enum.map(
-        fn tiles_in_scan_line ->
-          tiles_in_scan_line
-          |> Enum.reduce(
-            [],
-            fn tile_id, acc ->
-              tile_ch =
-                case tile_id do
-                  @empty -> " "
-                  @wall -> "#"
-                  @block -> "*"
-                  @paddle -> "^"
-                  @ball -> "O"
-                  x -> raise "unknown tile_id #{x}"
-                end
-
-              [tile_ch | acc]
-            end
-          )
-        end
-      )
-      |> Enum.map(&Enum.reverse/1)
-      |> Enum.intersperse("\n")
-    end
-
-    # auxiliary functions to share select constants with enclosing module
-    def coord_x, do: @do_coord_x
-    def coord_y, do: @do_coord_y
-    def paint, do: @do_paint
-
-    def empty, do: @empty
-    def wall, do: @wall
-    def block, do: @block
-    def paddle, do: @paddle
-    def ball, do: @ball
-  end
-
-  # ^^^
+  alias BreakoutArcade.WorldAffairs
+  import BreakoutArcade.WorldAffairs, only: [jl: 0, j_: 0, jr: 0]
 
   @doc """
   part 2
@@ -201,7 +209,7 @@ defmodule BreakoutArcade do
         tiles: %{},
         buffer: [],
         score: 0,
-        joy_moves: [:j_, :j_, :j_, :j_, :j_, :j_],
+        joy_moves: [jl, jl, jl, j_, j_, j_],
         next_action: WorldAffairs.coord_x()
       ]
     )
@@ -210,7 +218,7 @@ defmodule BreakoutArcade do
 
     kw = WorldAffairs.get_final_state()
 
-    #TODO
+    Keyword.fetch!(kw, :score)
   end
 
   @doc """
