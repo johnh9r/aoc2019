b087319b0f393ecd9f8ef8edc90a1025d5f8cc67
John Hagemeister <johnh9r@posteo.net>
2019-12-17 00:53:37 +0100

    d16p2: acc underflow :-(

--- day16/flawed_freq_tx/lib/flawed_freq_tx.ex
+++ day16/flawed_freq_tx/lib/flawed_freq_tx.ex
@@ -4,6 +4,8 @@ defmodule FlawedFreqTx do
   """
 
   @base_pattern [0, 1, 0, -1]
+  # ASCII
+  @minus 0x2d
 
   @doc """
   iex> FlawedFreqTx.run_extra_long_flawed_frequency_processing("03036732577212944063491565474664", 100)
@@ -43,22 +45,24 @@ defmodule FlawedFreqTx do
   "52432133" 
   """
   @spec run_flawed_frequency_processing(String.t(), integer, integer) :: String.t()
-  def run_flawed_frequency_processing(message, iterations, offset \\ 0)
-
-  def run_flawed_frequency_processing(message, iterations, offset) when iterations <= 0 do
+  def run_flawed_frequency_processing(message, iterations, offset \\ 0) do
     message
-    |> String.slice(offset, 8)
+    |> String.split(~r//, trim: true)
+    |> _run_flawed_frequency_processing(iterations, offset)
+    |> Enum.join("")
   end
 
-  def run_flawed_frequency_processing(message, iterations, offset) do
-    # IO.inspect(iterations, label: "\niter")
+  @spec _run_flawed_frequency_processing([String.t()], integer, integer) :: [String.t()]
+  defp _run_flawed_frequency_processing(message, iterations, offset) when iterations <= 0 do
+    message
+    |> Enum.drop(offset)
+    |> Enum.take(8)
+  end
 
-    message_digits =
-      message
-      |> String.split(~r//, trim: true)
-
+  defp _run_flawed_frequency_processing(message, iterations, offset) do
+    # IO.inspect({iterations, message}, label: "\niter", width: :infinity)
     phase_output_message =
-      message_digits
+      message
       |> Enum.with_index(1)
       # In each phase, a new list is constructed with the same length as the input list.
       |> Enum.map(
@@ -69,71 +73,280 @@ defmodule FlawedFreqTx do
             # in the output list being considered. Repeat once for the first element, [etc.]
             |> Enum.flat_map(fn x -> Stream.cycle([x]) |> Enum.take(i) end)
             |> Stream.cycle()
-            |> Stream.drop(1)
-
             # When applying the pattern, skip the very first value exactly once.
+            |> Stream.drop(1)
 
           # each element in the output array uses all of the same input array elements;
           # element in the new list is built by multiplying every value in the input list
           # by a value in a repeating pattern and then adding up the results
-          Stream.zip(message_digits, repeating_pattern)
+          Stream.zip(message, repeating_pattern)
           # |> IO.inspect(label: "\nzip(#{i})")
+          # https://elixir-lang.org/getting-started/case-cond-and-if.html#case
           |> Enum.reduce(
-            0,
-            fn {d, p}, acc ->
-              acc + least_significant_digit_mult(d, p)
+            "0",
+            fn
+              {"0", _}, acc -> acc
+
+              {_, 0}, acc -> acc
+
+              # negative value -- if any -- always in second arg;
+              # input digit always non-negative
+              {d, 1}, acc ->
+                least_significant_digit_add(d, acc)
+
+              {d, -1}, acc ->
+                # exploit commutativity and distributivity for shorter lookup table
+                # negative value -- if any -- always in second arg;
+                # input digit always non-negative
+                case acc do
+                  # NOTE -a - b = -(a + b)
+                  # XXX -9 -5 = -(9 + 5) = -12 ... -2
+                  #                   then -12 + 3 = -9
+                  #                    but  -2 + 3 = (3 + -2) = 1
+                  <<@minus::8, abs_acc_code::8>> ->
+                    case least_significant_digit_add(<<abs_acc_code>>, d) do
+                      # avoid "-0" ambiguity
+                      "0" -> "0"
+                       x -> "-#{x}"
+                    end
+
+                  _ ->
+                    least_significant_digit_add(acc, "-#{d}")
+                end
             end
           )
-          # only the ones digit is kept
-          |> Kernel.abs()
-          |> Integer.mod(10)
+          |> case do
+            <<@minus::8, digit_code::8>> -> <<digit_code>>
+            digit -> digit
+          end
         end
       )
-      |> Enum.join("")
 
     # This new list is also used as the input for the next phase.
-    run_flawed_frequency_processing(phase_output_message, iterations - 1, offset)
+    phase_output_message
+    # |> IO.inspect(label: "\nout")
+    |> _run_flawed_frequency_processing(iterations - 1, offset)
   end
 
   #
   # custom arithmetic to avoid expensive arbitrary-precision of standard library
+  # calculate only least-significant digit of addition/subtraction result (by lookup)
+  #
+  # https://en.wikipedia.org/wiki/Modulo_operation#Properties_(identities)
+  # mod is distributive over addition and multiplication
   #
 
-  # calculate only least-significant digit of multiplication result (by lookup)
-  @spec least_significant_digit_mult(String.t(), integer) :: integer
-  defp least_significant_digit_mult(digit_s, sign) do
-    case sign do
-      0 -> 0
-      1 ->
-        case digit_s do
-          "0" -> 0
-          "1" -> 1
-          "2" -> 2
-          "3" -> 3
-          "4" -> 4
-          "5" -> 5
-          "6" -> 6
-          "7" -> 7
-          "8" -> 8
-          "9" -> 9
-            c -> raise "unsupported digit in input message (#{c})"
-        end
-     -1 ->
-        case digit_s do
-          "0" ->  0
-          "1" -> -1
-          "2" -> -2
-          "3" -> -3
-          "4" -> -4
-          "5" -> -5
-          "6" -> -6
-          "7" -> -7
-          "8" -> -8
-          "9" -> -9
-            c -> raise "unsupported digit in input message (#{c})"
-        end
-      n -> raise "unsupported factor in pattern (#{n})"
+  @spec least_significant_digit_add(String.t(), String.t()) :: String.t()
+  defp least_significant_digit_add(digit, incr) do
+    case {digit, incr} do
+      {"0", "-9"} -> "-9"
+      {"1", "-9"} -> "-8"
+      {"2", "-9"} -> "-7"
+      {"3", "-9"} -> "-6"
+      {"4", "-9"} -> "-5"
+      {"5", "-9"} -> "-4"
+      {"6", "-9"} -> "-3"
+      {"7", "-9"} -> "-2"
+      {"8", "-9"} -> "-1"
+      {"9", "-9"} -> "0"
 
+      {"0", "-8"} -> "-8"
+      {"1", "-8"} -> "-7"
+      {"2", "-8"} -> "-6"
+      {"3", "-8"} -> "-5"
+      {"4", "-8"} -> "-4"
+      {"5", "-8"} -> "-3"
+      {"6", "-8"} -> "-2"
+      {"7", "-8"} -> "-1"
+      {"8", "-8"} -> "0"
+      {"9", "-8"} -> "1"
+
+      {"0", "-7"} -> "-7"
+      {"1", "-7"} -> "-6"
+      {"2", "-7"} -> "-5"
+      {"3", "-7"} -> "-4"
+      {"4", "-7"} -> "-3"
+      {"5", "-7"} -> "-2"
+      {"6", "-7"} -> "-1"
+      {"7", "-7"} -> "0"
+      {"8", "-7"} -> "1"
+      {"9", "-7"} -> "2"
+
+      {"0", "-6"} -> "-6"
+      {"1", "-6"} -> "-5"
+      {"2", "-6"} -> "-4"
+      {"3", "-6"} -> "-3"
+      {"4", "-6"} -> "-2"
+      {"5", "-6"} -> "-1"
+      {"6", "-6"} -> "0"
+      {"7", "-6"} -> "1"
+      {"8", "-6"} -> "2"
+      {"9", "-6"} -> "3"
+
+      {"0", "-5"} -> "-5"
+      {"1", "-5"} -> "-4"
+      {"2", "-5"} -> "-3"
+      {"3", "-5"} -> "-2"
+      {"4", "-5"} -> "-1"
+      {"5", "-5"} -> "0"
+      {"6", "-5"} -> "1"
+      {"7", "-5"} -> "2"
+      {"8", "-5"} -> "3"
+      {"9", "-5"} -> "4"
+
+      {"0", "-4"} -> "-4"
+      {"1", "-4"} -> "-3"
+      {"2", "-4"} -> "-2"
+      {"3", "-4"} -> "-1"
+      {"4", "-4"} -> "0"
+      {"5", "-4"} -> "1"
+      {"6", "-4"} -> "2"
+      {"7", "-4"} -> "3"
+      {"8", "-4"} -> "4"
+      {"9", "-4"} -> "5"
+
+      {"0", "-3"} -> "-3"
+      {"1", "-3"} -> "-2"
+      {"2", "-3"} -> "-1"
+      {"3", "-3"} -> "0"
+      {"4", "-3"} -> "1"
+      {"5", "-3"} -> "2"
+      {"6", "-3"} -> "3"
+      {"7", "-3"} -> "4"
+      {"8", "-3"} -> "5"
+      {"9", "-3"} -> "6"
+
+      {"0", "-2"} -> "-2"
+      {"1", "-2"} -> "-1"
+      {"2", "-2"} -> "0"
+      {"3", "-2"} -> "1"
+      {"4", "-2"} -> "2"
+      {"5", "-2"} -> "3"
+      {"6", "-2"} -> "4"
+      {"7", "-2"} -> "5"
+      {"8", "-2"} -> "6"
+      {"9", "-2"} -> "7"
+
+      {"0", "-1"} -> "-1"
+      {"1", "-1"} -> "0"
+      {"2", "-1"} -> "1"
+      {"3", "-1"} -> "2"
+      {"4", "-1"} -> "3"
+      {"5", "-1"} -> "4"
+      {"6", "-1"} -> "5"
+      {"7", "-1"} -> "6"
+      {"8", "-1"} -> "7"
+      {"9", "-1"} -> "8"
+
+      {"0", "0"} -> "0"
+      {"1", "0"} -> "1"
+      {"2", "0"} -> "2"
+      {"3", "0"} -> "3"
+      {"4", "0"} -> "4"
+      {"5", "0"} -> "5"
+      {"6", "0"} -> "6"
+      {"7", "0"} -> "7"
+      {"8", "0"} -> "8"
+      {"9", "0"} -> "9"
+
+      {"0", "1"} -> "1"
+      {"1", "1"} -> "2"
+      {"2", "1"} -> "3"
+      {"3", "1"} -> "4"
+      {"4", "1"} -> "5"
+      {"5", "1"} -> "6"
+      {"6", "1"} -> "7"
+      {"7", "1"} -> "8"
+      {"8", "1"} -> "9"
+      {"9", "1"} -> "0"
+
+      {"0", "2"} -> "2"
+      {"1", "2"} -> "3"
+      {"2", "2"} -> "4"
+      {"3", "2"} -> "5"
+      {"4", "2"} -> "6"
+      {"5", "2"} -> "7"
+      {"6", "2"} -> "8"
+      {"7", "2"} -> "9"
+      {"8", "2"} -> "0"
+      {"9", "2"} -> "1"
+
+      {"0", "3"} -> "3"
+      {"1", "3"} -> "4"
+      {"2", "3"} -> "5"
+      {"3", "3"} -> "6"
+      {"4", "3"} -> "7"
+      {"5", "3"} -> "8"
+      {"6", "3"} -> "9"
+      {"7", "3"} -> "0"
+      {"8", "3"} -> "1"
+      {"9", "3"} -> "2"
+
+      {"0", "4"} -> "4"
+      {"1", "4"} -> "5"
+      {"2", "4"} -> "6"
+      {"3", "4"} -> "7"
+      {"4", "4"} -> "8"
+      {"5", "4"} -> "9"
+      {"6", "4"} -> "0"
+      {"7", "4"} -> "1"
+      {"8", "4"} -> "2"
+      {"9", "4"} -> "3"
+
+      {"0", "5"} -> "5"
+      {"1", "5"} -> "6"
+      {"2", "5"} -> "7"
+      {"3", "5"} -> "8"
+      {"4", "5"} -> "9"
+      {"5", "5"} -> "0"
+      {"6", "5"} -> "1"
+      {"7", "5"} -> "2"
+      {"8", "5"} -> "3"
+      {"9", "5"} -> "4"
+
+      {"0", "6"} -> "6"
+      {"1", "6"} -> "7"
+      {"2", "6"} -> "8"
+      {"3", "6"} -> "9"
+      {"4", "6"} -> "0"
+      {"5", "6"} -> "1"
+      {"6", "6"} -> "2"
+      {"7", "6"} -> "3"
+      {"8", "6"} -> "4"
+      {"9", "6"} -> "5"
+
+      {"0", "7"} -> "7"
+      {"1", "7"} -> "8"
+      {"2", "7"} -> "9"
+      {"3", "7"} -> "0"
+      {"4", "7"} -> "1"
+      {"5", "7"} -> "2"
+      {"6", "7"} -> "3"
+      {"7", "7"} -> "4"
+      {"8", "7"} -> "5"
+      {"9", "7"} -> "6"
+
+      {"0", "8"} -> "8"
+      {"1", "8"} -> "9"
+      {"2", "8"} -> "0"
+      {"3", "8"} -> "1"
+      {"4", "8"} -> "2"
+      {"5", "8"} -> "3"
+      {"6", "8"} -> "4"
+      {"7", "8"} -> "5"
+      {"8", "8"} -> "6"
+      {"9", "8"} -> "7"
+
+      {"0", "9"} -> "9"
+      {"1", "9"} -> "0"
+      {"2", "9"} -> "1"
+      {"3", "9"} -> "2"
+      {"4", "9"} -> "3"
+      {"5", "9"} -> "4"
+      {"6", "9"} -> "5"
+      {"7", "9"} -> "6"
+      {"8", "9"} -> "7"
+      {"9", "9"} -> "8"
     end
   end
 end
